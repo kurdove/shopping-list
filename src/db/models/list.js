711d746dd7d981329aa9cb4af77f9b7046cd2b8e
Evgheni Curdov <kurdov.e@gmail.com>
2019-03-24 20:58:11 -0700

    Database initiated

---
+++ src/db/models/list.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var List = sequelize.define('List', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {});
+  List.associate = function(models) {
+    // associations can be defined here
+  };
+  return List;
+};
