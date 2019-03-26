0d9a074461e9dce4243dade58160f6984320b0fb
Evgheni Curdov <kurdov.e@gmail.com>
2019-03-25 21:39:25 -0700

    list CRUD in progress

--- spec/integration/lists_spec.js
+++ spec/integration/lists_spec.js
@@ -11,8 +11,8 @@ describe("routes : lists", () => {
         sequelize.sync({force: true}).then((res) => {
     
             List.create({
-                title: "JS Frameworks",
-                description: "There is a lot of them"
+                title: "Test Shopping List",
+                description: "Grocery list for the next shopping"
             })
             .then((list) => {
                 this.list = list;
@@ -30,15 +30,89 @@ describe("routes : lists", () => {
     describe("GET /lists", () => {
 
         it("should return a status code 200 and all lists", (done) => {
-
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(err).toBeNull();
                 expect(body).toContain("Lists");
-                expect(body).toContain("JS Frameworks");
+                expect(body).toContain("Test Shopping List");
                 done();
             });
         });
 
+    });
+
+    describe("GET /lists/new", () => {
+
+        it("should render a new list form", (done) => {
+            request.get(`${base}new`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("New List");
+                done();
+            });
+        });
+    
+    });
+
+    describe("POST /lists/create", () => {
+        const options = {
+            url: `${base}create`,
+            form: {
+                title: "Target shopping list",
+                description: "essencials for the camping"
+            }
+        };
+  
+        it("should create a new list and redirect", (done) => {
+            request.post(options,
+                (err, res, body) => {
+                List.findOne({where: {title: "Target shopping list"}})
+                .then((list) => {
+                    expect(res.statusCode).toBe(303);
+                    expect(list.title).toBe("Target shopping list");
+                    expect(list.description).toBe("essencials for the camping");
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+                }
+            );
+        });
+    });
+
+    describe("GET /lists/:id", () => {
+
+        it("should render a view with the selected list", (done) => {
+            request.get(`${base}${this.list.id}`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Test Shopping List");
+                done();
+            });
+        });
+   
+    });
+
+    describe("POST /lists/:id/destroy", () => {
+
+        it("should delete the list with the associated ID", (done) => {
+            List.findAll()
+            .then((lists) => {
+                const listCountBeforeDelete = lists.length;
+    
+                expect(listCountBeforeDelete).toBe(1);
+                request.post(`${base}${this.list.id}/destroy`, (err, res, body) => {
+                    List.findAll()
+                    .then((lists) => {
+                        expect(err).toBeNull();
+                        expect(lists.length).toBe(listCountBeforeDelete - 1);
+                        done();
+                    })
+    
+                });
+            });
+   
+        });
+   
     });
 });
--- src/db/queries.lists.js
+++ src/db/queries.lists.js
@@ -9,5 +9,40 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
+  },
+
+  getList(id, callback){
+    return List.findByPk(id)
+    .then((list) => {
+      callback(null, list);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  addList(newList, callback){
+    return List.create({
+      title: newList.title,
+      description: newList.description
+    })
+    .then((list) => {
+      callback(null, list);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  deleteList(id, callback){
+    return List.destroy({
+      where: {id}
+    })
+    .then((list) => {
+      callback(null, list);
+    })
+    .catch((err) => {
+      callback(err);
+    })
   }
 }
